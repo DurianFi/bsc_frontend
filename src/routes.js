bfe594fa323d05f952d2b33859a71b6f078e2216
pengchew <chewzooka@gmail.com>
2021-11-10 01:15:14 +0800

    1

--- src/routes.js
+++ src/routes.js
@@ -22,11 +22,11 @@ export default function Router(prop) {
       element: <DashboardLayout key={wallet}  wallet={prop.wallet} init={prop.init} />,
       children: [
         { element: <Navigate to="/dashboard" replace /> },
-        { path: 'dashboard', element: <DashboardApp key={wallet}  wallet={prop.wallet} init={prop.init}/> },
-        { path: 'login', element: <Login key={wallet}  wallet={prop.wallet} init={prop.init}/> },
-        { path: '404', element: <NotFound key={wallet}  wallet={prop.wallet} init={prop.init}/> },
-        { path: 'swapandstake', element: <Swapandstake key={wallet}  wallet={prop.wallet} init={prop.init}/> },
-        { path: 'stake', element: <Stake key={wallet}  wallet={prop.wallet} init={prop.init}/> },
+        { path: '/dashboard', element: <DashboardApp key={wallet}  wallet={prop.wallet} init={prop.init}/> },
+        { path: '/login', element: <Login key={wallet}  wallet={prop.wallet} init={prop.init}/> },
+        { path: '/404', element: <NotFound key={wallet}  wallet={prop.wallet} init={prop.init}/> },
+        { path: '/swapandstake', element: <Swapandstake key={wallet}  wallet={prop.wallet} init={prop.init}/> },
+        { path: '/stake', element: <Stake key={wallet}  wallet={prop.wallet} init={prop.init}/> },
         { path: '*', element: <Navigate to="/404" key={wallet}  wallet={prop.wallet} init={prop.init}/> }
       ]
     },
